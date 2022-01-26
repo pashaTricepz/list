c9bf12c43dbc1cf3f37272d80c551e6daed118d7
pasha <pavel.bahtin2016@yandex.ru>
2022-01-26 18:40:29 +0300

    from 1 to 10

--- main.cpp
+++ main.cpp
@@ -5,15 +5,12 @@
 
 int main() {
 	MyList<int> l;;
-	srand(time(0)); 
-	for (int i = 1; i <= 20; i++) {
-		l.push_back(i);
+	for (int i = 0; i < 10; ++i) {
+		l.push_back(i + 1);
 	}
-	std::cout << "My list: ";
 	for (MyList<int>::iterator it = l.begin(); it != l.end(); ++it) {
 		std::cout << *it << " ";
 	}
 	std::cout << '\n';
-
 	return 0;
 }
